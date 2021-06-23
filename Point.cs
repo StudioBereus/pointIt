ba6d9df83888157e79dffd8eeac12c6656bcac7d
osalysdecatarina <72741076+osalysdecatarina@users.noreply.github.com>
2021-06-23 15:12:57 +0200

    Ajoutez des fichiers projet.

---
+++ Point.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pointIt
+{
+    class Point
+    {
+        //Attributs
+        public double positionX;
+        public double positionY;
+
+        //Constructeurs
+        public Point(){
+            positionX = 0;
+            positionY = 0;
+        }
+        public Point(int AxeX, int AxeY){
+            positionX = AxeX;
+            positionY = AxeY;
+        }
+        public void swap(Point point2){
+            //X
+            double temPosX = this.positionX;
+            this.positionX = point2.positionX;
+            point2.positionX = temPosX;
+
+            //Y
+            double temPosY = this.positionY;
+            this.positionY = point2.positionY;
+            point2.positionY = temPosY;
+        }
+        public void moveX(double distance) {
+            positionX += distance;
+            Console.WriteLine("Le point se trouve à présent aux coordonnées (" + positionX + ";" + positionY + ")");
+        }
+        public void moveY(double distance){
+            positionY += distance;
+            Console.WriteLine("Le point se trouve à présent aux coordonnées (" + positionX + ";" + positionY + ")");
+        }
+        public void moveAll(double distanceX, double distanceY){
+            this.moveX(distanceX);
+            this.moveY(distanceY);
+            Console.WriteLine("Le point se trouve à présent aux coordonnées (" + positionX + ";" + positionY + ")");
+        }
+        public void symX(){
+            this.positionY *= -1;
+            Console.WriteLine("("+positionX + " ; " + positionY+")");
+        }
+        public void symY(){
+            this.positionY *= -1;
+        }
+        public void symOr(){
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pointIt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Point p1 = new Point(47, 24);
+            p1.moveX(12);
+            p1.symX();
+            float x = 20f / 3f;
+            Console.WriteLine(x);
+        }
+    }
+}
